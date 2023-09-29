b0cf40e76c8bd1c7a0ccb544e0a117c0824e7a85
Wolfxela <alexthegreitsas@gmail.com>
2023-09-29 23:20:15 +0300

    added all needed js files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,14 @@ const path = require('path');
 
 module.exports = 
 {
-    entry: './src/index.js',
+    entry:{
+         index:'./src/index.js',
+         homePage:'./src/homePage.js',
+         menuPage:'./src/menuPage.js',
+         contactPage:'./src/contactPage.js',
+         aboutUsPage:'./src/aboutUsPage.js',
+    },
+
     output:
     {
         filename: '[name].js',
